2086db155bf03e169f1b7eaa293e6d805df14f19
Valerio Barbera <ilvalerione@gmail.com>
2025-03-05 14:40:46 +0100

    open ai function calls

--- src/Providers/Mistral.php
+++ src/Providers/Mistral.php
@@ -2,13 +2,6 @@
 
 namespace NeuronAI\Providers;
 
-use GuzzleHttp\RequestOptions;
-use NeuronAI\Exceptions\ProviderException;
-use NeuronAI\Chat\Messages\AssistantMessage;
-use NeuronAI\Chat\Messages\Message;
-use GuzzleHttp\Client;
-use NeuronAI\Chat\Messages\UserMessage;
-
 class Mistral extends OpenAI
 {
     protected string $baseUri = 'https://api.mistral.ai';
