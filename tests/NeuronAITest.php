d9c1533305b1c45b33799f7231c080a4b37dfe69
Valerio Barbera <ilvalerione@gmail.com>
2025-03-09 19:38:54 +0100

    fix tool call message

--- tests/NeuronAITest.php
+++ tests/NeuronAITest.php
@@ -5,7 +5,13 @@ namespace NeuronAI\Tests;
 
 use NeuronAI\Agent;
 use NeuronAI\AgentInterface;
+use NeuronAI\Chat\Messages\AssistantMessage;
+use NeuronAI\Chat\Messages\Message;
+use NeuronAI\Chat\Messages\UserMessage;
 use NeuronAI\RAG\RAG;
+use NeuronAI\Tools\Tool;
+use NeuronAI\Tools\ToolCallMessage;
+use NeuronAI\Tools\ToolInterface;
 use PHPUnit\Framework\TestCase;
 
 class NeuronAITest extends TestCase
@@ -27,5 +33,22 @@ class NeuronAITest extends TestCase
 
         $neuron = new RAG();
         $this->assertInstanceOf(Agent::class, $neuron);
+    }
+
+    public function testMessageInstance()
+    {
+        $tools = [
+            new Tool('example', 'example')
+        ];
+
+        $this->assertInstanceOf(Message::class, new UserMessage(''));
+        $this->assertInstanceOf(Message::class, new AssistantMessage(''));
+        $this->assertInstanceOf(Message::class, new ToolCallMessage('', $tools));
+    }
+
+    public function testToolInstance()
+    {
+        $tool = new Tool('example', 'example');
+        $this->assertInstanceOf(ToolInterface::class, $tool);
     }
 }
