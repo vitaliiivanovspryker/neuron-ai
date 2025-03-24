8244971dcb8eb3c8d7743c4a065ebb93c61ac6d3
Valerio Barbera <ilvalerione@gmail.com>
2025-03-24 16:21:21 +0100

    fix test

--- tests/DataLoaderTest.php
+++ tests/DataLoaderTest.php
@@ -7,9 +7,11 @@ use PHPUnit\Framework\TestCase;
 
 class DataLoaderTest extends TestCase
 {
-    public function testMessage()
+    public function testStringDataLoader()
     {
-        $result = StringDataLoader::for('Hello')->getDocuments();
-        $this->assertIsArray($result);
+        $documents = StringDataLoader::for('test')->getDocuments();
+        $this->assertCount(1, $documents);
+        $this->assertEquals('test', $documents[0]->content);
+        $this->assertEquals(\hash('sha256', 'test'), $documents[0]->hash);
     }
 }
