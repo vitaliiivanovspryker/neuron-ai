10f51b869bd1dc159ec7033cb0995cbac06fe5f3
Alexandre <alexandre.cavanna.pro@gmail.com>
2025-04-19 19:19:35 +0200

    feat: add doctrine vector store

---
+++ src/RAG/VectorStore/Doctrine/PostgresqlVectorL2OperatorDql.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace NeuronAI\RAG\VectorStore\Doctrine;
+
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * L2DistanceFunction ::= "L2_DISTANCE" "(" VectorPrimary "," VectorPrimary ")"
+ */
+final class PostgresqlVectorL2OperatorDql extends AbstractDBL2OperatorDql
+{
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'L2_DISTANCE('.
+            $this->vectorOne->dispatch($sqlWalker).', '.
+            $this->vectorTwo->dispatch($sqlWalker).
+            ')';
+    }
+}
