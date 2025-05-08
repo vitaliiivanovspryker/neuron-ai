5677e907f130c3bfd798c5db83c3c6c716b8b025
Valerio Barbera <ilvalerione@gmail.com>
2025-05-08 16:57:06 +0200

    web search

---
+++ src/Tools/Components/TavilySearchTool.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace NeuronAI\Tools\Components;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use NeuronAI\Tools\Tool;
+use NeuronAI\Tools\ToolProperty;
+
+class TavilySearchTool extends Tool
+{
+    protected Client $client;
+
+    protected string $utl = 'https://api.tavily.com/v1/';
+
+    protected array $options = [
+        'search_depth' => 'basic',
+        'chunks_per_source' => 3,
+        'max_results' => 1,
+    ];
+
+    /**
+     * @param string $key Tavily API key.
+     * @param array $topics Explicit the topics you want to force the Agent to perform web search.
+     */
+    public function __construct(
+        protected string $key,
+        protected array $topics = [],
+    ) {
+        parent::__construct(
+            'web_search',
+            'Use this tool to search the web for additional information '.
+            (!empty($this->topics) ? 'about '.implode(', ', $this->topics).', or ' : '').
+            'if the question is outside the scope of the context you have.'
+        );
+
+        $this->initTool();
+        $this->client = new Client([
+            'base_uri' => trim($this->utl, '/').'/',
+            'headers' => [
+                'Authorization' => 'Bearer '.$this->key,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ]
+        ]);
+    }
+
+    protected function initTool()
+    {
+        $this->addProperty(
+            new ToolProperty(
+                'search_query',
+                'string',
+                'The search query to perform web search.',
+                true),
+        )->addProperty(
+            new ToolProperty(
+                'topic',
+                'string',
+                'Explicit the topic you want to perform the web search on.',
+                false),
+        )->addProperty(
+            new ToolProperty(
+                'time_range',
+                'string',
+                '',
+                false,
+                ['day, week, month, year']
+            )
+        )->addProperty(
+            new ToolProperty(
+                'days',
+                'integer',
+                '',
+                false,
+            )
+        );
+
+        $this->setCallable(
+            fn(
+                string $search_query,
+                ...$args
+            ) => $this->search(
+                $search_query,
+                ...$args
+            )
+        );
+    }
+
+    protected function search(
+        string $search_query,
+        string $topic = null,
+        string $time_range = 'day',
+        int $days = 7,
+    ) {
+        $result = $this->client->post('search', [
+            RequestOptions::JSON => \array_merge(
+                compact('topic', 'time_range', 'days'),
+                $this->options,
+                [
+                    'query' => $search_query,
+                ]
+            )
+        ])->getBody()->getContents();
+
+        $result = \json_decode($result, true);
+
+        return [
+            'answer' => $result['answer'],
+            'results' => \array_map(fn($item) => [
+                'title' => $item['title'],
+                'url' => $item['url'],
+                'content' => $item['content'],
+            ], $result['results']),
+        ];
+    }
+
+    public function withOptions(array $options): self
+    {
+        $this->options = $options;
+        return $this;
+    }
+}
