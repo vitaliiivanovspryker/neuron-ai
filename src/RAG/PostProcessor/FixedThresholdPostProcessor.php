795e7a15d94107d95063c17f95406b2078e0a28f
Alessandro Astarita <aleast@capri.it>
2025-05-15 14:52:28 +0100

    Added fixed threshold post processor implementation

---
+++ src/RAG/PostProcessor/FixedThresholdPostProcessor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NeuronAI\RAG\PostProcessor;
+
+use NeuronAI\Chat\Messages\Message;
+use NeuronAI\RAG\Document;
+
+class FixedThresholdPostProcessor implements PostProcessorInterface
+{
+    /**
+     * The threshold value below which documents will be filtered out.
+     *
+     * @var float
+     */
+    protected float $threshold;
+
+    /**
+     * Creates a post processor that filters documents based on a fixed threshold.
+     *
+     * @param float $threshold threshold value (documents with scores below this value will be filtered out)
+     */
+    public function __construct(float $threshold = 0.5)
+    {
+        $this->threshold = $threshold;
+    }
+
+    /**
+     * Filters documents using a fixed threshold value.
+     *
+     * @param Message $question
+     * @param Document[] $documents
+     * @return Document[]
+     */
+    public function process(Message $question, array $documents): array
+    {
+        return \array_values(\array_filter($documents, function ($document) {
+            return $document->score >= $this->threshold;
+        }));
+    }
+}
