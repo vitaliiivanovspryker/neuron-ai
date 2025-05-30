aa5b07fcb7c73285028f34cac1cb93aadbd4a92e
Valerio Barbera <valerio@inspector.dev>
2025-05-30 18:50:25 +0200

    fix meilisearch

--- src/RAG/VectorStore/MeilisearchVectorStore.php
+++ src/RAG/VectorStore/MeilisearchVectorStore.php
@@ -11,13 +11,13 @@ class MeilisearchVectorStore implements VectorStoreInterface
     protected Client $client;
 
     public function __construct(
-        protected string $indexUid,
-        protected string $host = 'http://localhost:7700',
+        string $indexUid,
+        string $host = 'http://localhost:7700',
         ?string $key = null,
         protected int $topK = 5,
     ) {
         $this->client = new Client([
-            'base_uri' => trim($this->host, '/').'/indexes/'.$this->indexUid.'/',
+            'base_uri' => trim($host, '/').'/indexes/'.$indexUid.'/',
             'headers' => [
                 'Content-Type' => 'application/json',
                 ...(!is_null($key) ? ['Authorization' => "Bearer {$key}"] : [])
@@ -27,9 +27,9 @@ class MeilisearchVectorStore implements VectorStoreInterface
         try {
             $this->client->get('');
         } catch (\Exception $exception) {
-            $this->client->post(trim($this->host, '/').'/indexes/', [
+            $this->client->post(trim($host, '/').'/indexes/', [
                 RequestOptions::JSON => [
-                    'uid' => $this->indexUid,
+                    'uid' => $indexUid,
                     'primaryKey' => 'id',
                 ]
             ]);
