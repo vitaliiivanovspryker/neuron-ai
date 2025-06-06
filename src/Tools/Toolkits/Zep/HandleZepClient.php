1ab30bf9225c7383d44b9cf0ca6adb5892ad8b93
Valerio Barbera <valerio@inspector.dev>
2025-06-06 17:36:01 +0200

    zep toolkit

--- src/Tools/Toolkits/Zep/HandleZepClient.php
+++ src/Tools/Toolkits/Zep/HandleZepClient.php
@@ -38,26 +38,4 @@ trait HandleZepClient
 
         return $this;
     }
-
-    protected function createSession(): self
-    {
-        // @phpstan-ignore property.notFound
-        if (is_null($this->session_id)) {
-            $this->session_id = \uniqid();
-        }
-
-        // Create the session if it doesn't exist
-        try {
-            $this->client->get('sessions/' . $this->session_id);
-        } catch (\Exception $exception) {
-            $this->client->post('sessions', [
-                RequestOptions::JSON => [
-                    'session_id' => $this->session_id,
-                    'user_id' => $this->user_id,
-                ]
-            ]);
-        }
-
-        return $this;
-    }
 }
