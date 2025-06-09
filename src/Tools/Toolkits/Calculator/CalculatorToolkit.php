98ac6bffcbbbb58eede36fa4f492632464fb56db
Valerio Barbera <valerio@inspector.dev>
2025-06-09 12:18:41 +0200

    Bootstrap Toolkit

--- src/Tools/Toolkits/Calculator/CalculatorToolkit.php
+++ src/Tools/Toolkits/Calculator/CalculatorToolkit.php
@@ -6,6 +6,12 @@ use NeuronAI\Tools\Toolkits\AbstractToolkit;
 
 class CalculatorToolkit extends AbstractToolkit
 {
+    public function guidelines(): ?string
+    {
+        return "This toolkit allows you to perform mathematical operations. You can also use this functions  solve
+        e mathematical expression executing smaller operations step by step to calculate the final result.";
+    }
+
     public function tools(): array
     {
         return [
