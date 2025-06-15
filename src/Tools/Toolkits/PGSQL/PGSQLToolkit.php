9f931966edcc2533c529c2728bb436e589c0e0b4
Valerio Barbera <valerio@inspector.dev>
2025-06-15 15:18:36 +0200

    pgsql toolkit

---
+++ src/Tools/Toolkits/PGSQL/PGSQLToolkit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NeuronAI\Tools\Toolkits\PGSQL;
+
+use NeuronAI\Tools\Toolkits\AbstractToolkit;
+use PDO;
+
+class PGSQLToolkit extends AbstractToolkit
+{
+    public function __construct(protected PDO $pdo)
+    {
+    }
+
+    public function guidelines(): ?string
+    {
+        return "These tools allow you to learn the database structure,
+        getting detailed information about tables, columns, relationships, and constraints
+        to generate and execute precise and efficient SQL queries.";
+    }
+
+    public function provide(): array
+    {
+        return [
+            PGSQLSchemaTool::make($this->pdo),
+            PGSQLSelectTool::make($this->pdo),
+        ];
+    }
+}
