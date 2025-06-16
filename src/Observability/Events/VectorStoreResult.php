35407fd0e59329e84321d497b3c685bda642811b
Valerio Barbera <valerio@inspector.dev>
2025-06-16 14:32:37 +0200

    document metadata

--- src/Observability/Events/VectorStoreResult.php
+++ src/Observability/Events/VectorStoreResult.php
@@ -3,12 +3,12 @@
 namespace NeuronAI\Observability\Events;
 
 use NeuronAI\Chat\Messages\Message;
-use NeuronAI\RAG\DocumentModelInterface;
+use NeuronAI\RAG\Document;
 
 class VectorStoreResult
 {
     /**
-     * @param DocumentModelInterface[] $documents
+     * @param Document[] $documents
      */
     public function __construct(
         public Message $question,
