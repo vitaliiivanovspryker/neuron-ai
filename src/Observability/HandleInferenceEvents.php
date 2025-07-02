685583fb628792501d920671a7f1392ab577a835
trippo <497169+trippo@users.noreply.github.com>
2025-07-02 07:32:26 +0000

    Apply PHP-CS-Fixer changes

--- src/Observability/HandleInferenceEvents.php
+++ src/Observability/HandleInferenceEvents.php
@@ -35,13 +35,15 @@ trait HandleInferenceEvents
         }
 
         $segment = $this->segments[$id];
-        $segment->addContext('Message', \array_merge($data->message->jsonSerialize(),
+        $segment->addContext('Message', \array_merge(
+            $data->message->jsonSerialize(),
             $data->message->getUsage() instanceof Usage ? [
                 'usage' => [
                     'input_tokens' => $data->message->getUsage()->inputTokens,
                     'output_tokens' => $data->message->getUsage()->outputTokens,
                 ]
-            ] : []));
+            ] : []
+        ));
         $segment->end();
     }
 
