1740d62cf58fddb566217c1f0d72124678f1b096
Valerio Barbera <valerio@inspector.dev>
2025-07-05 23:07:29 +0200

    deepseek compatibility

--- src/Providers/Deepseek/Deepseek.php
+++ src/Providers/Deepseek/Deepseek.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace NeuronAI\Providers\Deepseek;
 
+use NeuronAI\Chat\Messages\Message;
 use NeuronAI\Providers\OpenAI\OpenAI;
 
 class Deepseek extends OpenAI
 {
-    use HandleStructured;
-
     protected string $baseUri = "https://api.deepseek.com/v1";
+
+    public function structured(
+        array $messages,
+        string $class,
+        array $response_format
+    ): Message {
+        $this->parameters = \array_merge($this->parameters, [
+            'response_format' => [
+                'type' => 'json_object',
+            ]
+        ]);
+
+        $this->system .= \PHP_EOL."# OUTPUT FORMAT CONSTRAINTS".\PHP_EOL
+            .'Generate a json respecting this schema: '.\json_encode($response_format);
+
+        return $this->chat($messages);
+    }
 }
