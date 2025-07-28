b2dc48627ef977951e8cc0801759f54f8cd9c0e0
Victor R <39545521+viicslen@users.noreply.github.com>
2025-07-28 13:13:43 -0400

    Add tests for QdrantVectorStore including setup and teardown methods

--- tests/VectorStore/QdrantVectorStoreTest.php
+++ tests/VectorStore/QdrantVectorStoreTest.php
@@ -6,56 +6,78 @@ namespace NeuronAI\Tests\VectorStore;
 
 use NeuronAI\RAG\Document;
 use NeuronAI\RAG\VectorStore\QdrantVectorStore;
+use NeuronAI\Tests\Traits\CheckOpenPort;
 use PHPUnit\Framework\TestCase;
 
 class QdrantVectorStoreTest extends TestCase
 {
-    public function test_store_documents(): void
+    use CheckOpenPort;
+
+    protected QdrantVectorStore $store;
+
+    public function setUp(): void
+    {
+        if (!$this->isPortOpen('127.0.0.1', 6333)) {
+            $this->markTestSkipped('Port 6333 is not open. Skipping test.');
+        }
+
+        $this->store = new QdrantVectorStore('http://127.0.0.1:6333/collections/neuron-ai', '');
+        $this->store->initialize(3, 'Cosine', true);
+    }
+
+    public function tearDown(): void
+    {
+        $this->store->destroy();
+    }
+
+    public function test_add_document_and_search(): void
+    {
+        $document = new Document('Hello World!');
+        $document->addMetadata('customProperty', 'customValue');
+        $document->embedding = [1, 2, 3];
+        $document->id = 1;
+
+        $this->store->addDocument($document);
+
+        $results = $this->store->similaritySearch([1, 2, 3]);
+
+        $this->assertEquals($document->getContent(), $results[0]->getContent());
+        $this->assertEquals($document->metadata['customProperty'], $results[0]->metadata['customProperty']);
+    }
+
+    public function test_add_multiple_document_and_search(): void
     {
         $document = new Document('Hello!');
         $document->addMetadata('customProperty', 'customValue');
         $document->embedding = [1, 2, 3];
         $document->id = 1;
-        $document->sourceName = 'test';
-        $document->sourceType = 'string';
 
         $document2 = new Document('Hello 2!');
         $document2->embedding = [3, 4, 5];
         $document2->id = 2;
-        $document2->sourceName = 'test';
-        $document2->sourceType = 'string';
 
-        $store = new QdrantVectorStore('http://localhost:6333/collections/neuron-ai', '');
+        $this->store->addDocuments([$document, $document2]);
 
-        $store->initialize(3, 'Cosine');
-        $store->addDocuments([$document, $document2]);
+        $results = $this->store->similaritySearch([1, 2, 3]);
 
-        $results = $store->similaritySearch([1, 2, 3]);
-
-        $this->assertCount(1, $results);
-        $this->assertEquals($document->id, $results[0]->getId());
-        $this->assertEquals($document->content, $results[0]->getContent());
-        $this->assertEquals($document->embedding, $results[0]->getEmbedding());
-        $this->assertEquals($document->sourceType, $results[0]->getSourceType());
-        $this->assertEquals($document->sourceName, $results[0]->getSourceName());
-        $this->assertEquals($document->metadata, $results[0]->metadata);
+        $this->assertEquals($document->getContent(), $results[0]->getContent());
+        $this->assertEquals($document->metadata['customProperty'], $results[0]->metadata['customProperty']);
     }
 
     public function test_delete_documents(): void
     {
         $document = new Document('Hello!');
         $document->embedding = [1, 2, 3];
+        $document->id = 1;
 
         $document2 = new Document('Hello 2!');
         $document2->embedding = [3, 4, 5];
+        $document2->id = 2;
 
-        $store = new QdrantVectorStore('http://localhost:6333/collections/neuron-ai', '');
+        $this->store->addDocuments([$document, $document2]);
+        $this->store->deleteBySource('manual', 'manual');
 
-        $store->initialize(3, 'Cosine');
-        $store->addDocuments([$document, $document2]);
-        $store->deleteBySource('manual', 'manual');
-
-        $results = $store->similaritySearch([1, 2, 3]);
+        $results = $this->store->similaritySearch([1, 2, 3]);
         $this->assertCount(0, $results);
     }
 }
